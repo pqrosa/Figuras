eaf68e9d2e63c4c88a622b94717bdb8fc04124bc
pqrosa <pqrosa@o2c.tv.br>
2013-03-20 15:28:42 -0300

    Primeiro commit.

---
+++ Figuras/Quadrado.h
@@ -0,0 +1,13 @@
+//
+//  Quadrado.h
+//  Figuras
+//
+//  Created by Philippe Rosa on 3/20/13.
+//  Copyright (c) 2013 O2C Hipermídia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Quadrado : NSObject
+
+@end
---
+++ Figuras/Quadrado.m
@@ -0,0 +1,13 @@
+//
+//  Quadrado.m
+//  Figuras
+//
+//  Created by Philippe Rosa on 3/20/13.
+//  Copyright (c) 2013 O2C Hipermídia. All rights reserved.
+//
+
+#import "Quadrado.h"
+
+@implementation Quadrado
+
+@end
